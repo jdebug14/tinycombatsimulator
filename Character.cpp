13815f799acefdadbbd93435aeb021accbc7ed2a
Joel DeLong <joel.delong14@gmail.com>
2026-02-20 19:11:32 -0800

    add support for player level up

--- Character.cpp
+++ Character.cpp
@@ -2,57 +2,83 @@
 #include "CombatLogger.h"
 
 Character::Character(const std::string& name, int lvl, int hp, int ap, int dex) 
-    : name(name),
-    level(lvl),
-    health_points(hp),
-    attack_power(ap),
-    dexterity(dex) {}
-Player::Player(const std::string& name) : Character(name, 1, 5, 1, 1) {}
-Player::Player(const std::string& name, int lvl, int hp, int ap, int dex) : Character(name, lvl, hp, ap, dex) {}
-Enemy::Enemy(const std::string& name) : Character(name, 1, 1, 1, 1) {}
-Enemy::Enemy(const std::string& name, int lvl, int hp, int ap, int dex) : Character(name, lvl, hp, ap, dex) {}
+    : name_(name),
+    level_(lvl),
+    healthPoints_(hp),
+    attackPower_(ap),
+    dexterity_(dex) {}
 
 const std::string& Character::getName() const {
-    return name;
+    return name_;
 }
 
 int Character::getHealthPoints() const {
-    return health_points;
+    return healthPoints_;
 }
 
 int Character::getLevel() const {
-    return level;
+    return level_;
 }
 
 int Character::getAttackPower() const {
-    return attack_power;
+    return attackPower_;
 }
 
 int Character::getDexterity() const {
-    return dexterity;
+    return dexterity_;
 }
 
 bool Character::isAlive() const {
-    return health_points > 0;
+    return healthPoints_ > 0;
 }
 
 void Character::takeDamage(int d) {
-    health_points -= d;
-    if (health_points < 0) health_points = 0;
+    healthPoints_ -= d;
+    if (healthPoints_ < 0) healthPoints_ = 0;
 }
 
 void Character::attack(Character& target, int turn) {
     if (!isAlive()) return;
-    int damage = attack_power;
+    int damage = attackPower_;
     target.takeDamage(damage);
     CombatLogger::logAttack(*this, turn, target, damage);
 }
 
 std::ostream& operator<<(std::ostream& os, const Character& c) {
-    os << "Name: " << c.name << "\n"
-       << "Level: " << c.level << "\n"
-       << "HP: " << c.health_points << "\n"
-       << "Attack: " << c.attack_power << "\n"
-       << "Dexterity: " << c.dexterity << "\n";
+    os << "Name: " << c.name_ << "\n"
+       << "Level: " << c.level_ << "\n"
+       << "HP: " << c.healthPoints_ << "\n"
+       << "Attack: " << c.attackPower_ << "\n"
+       << "Dexterity: " << c.dexterity_ << "\n";
     return os;
+}
+
+// Player
+Player::Player(const std::string& name) : Character(name, 1, 5, 1, 1) {}
+Player::Player(const std::string& name, int lvl, int hp, int ap, int dex) : Character(name, lvl, hp, ap, dex) {}
+
+void Player::levelUp() {
+    if (!isAlive()) return;
+    level_++;
+    healthPoints_ += 3;
+    attackPower_ += 1;
+    dexterity_ += 1;
+    experienceToNextLevel_ += 50;
+    std::cout << name_ << " leveled up to " << level_ << "!\n";
+}
+
+void Player::gainExperience(int exp) {
+    if (!isAlive()) return;
+    experience_ += exp;
+    while (experience_ + exp >= experienceToNextLevel_) {
+        levelUp();
+    } 
+}
+
+// Enemy
+Enemy::Enemy(const std::string& name) : Character(name, 1, 1, 1, 1) {}
+Enemy::Enemy(const std::string& name, int lvl, int hp, int ap, int dex) : Character(name, lvl, hp, ap, dex) {}
+
+int Enemy::getExperienceGranted() const {
+    return experienceGranted_;
 }
--- Character.h
+++ Character.h
@@ -4,31 +4,49 @@
 
 struct Character {
     Character(const std::string& name, int lvl, int hp, int ap, int dex);
+    virtual ~Character() = default;
+
     const std::string& getName() const;
     int getLevel() const;
     int getHealthPoints() const;
     int getAttackPower() const;
     int getDexterity() const;
-    void attack(Character& target, int turn);
     void takeDamage(int d);
     bool isAlive() const;
+    void attack(Character& target, int turn);
+
+
+    protected:
+        std::string name_;
+        int level_;
+        int healthPoints_;
+        int attackPower_;
+        int dexterity_;
 
     friend std::ostream& operator<<(std::ostream& os, const Character& c);
-
-    private:
-        std::string name;
-        int level;
-        int health_points;
-        int attack_power;
-        int dexterity;
 };
 
 struct Player : Character {
     Player(const std::string& name);
     Player(const std::string& name, int lvl, int hp, int ap, int dex);
+
+    int getExperience() const;
+    int getExperienceToNextLevel() const;
+    void gainExperience(int exp);
+    void levelUp();
+
+    private:
+        int experience_;
+        int experienceToNextLevel_;
+
 };
 
 struct Enemy : Character {
     Enemy(const std::string& name);
     Enemy(const std::string& name, int lvl, int hp, int ap, int dex);
+
+    int getExperienceGranted() const;
+
+    private:
+        int experienceGranted_;
 };
